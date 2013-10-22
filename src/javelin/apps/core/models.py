6958a382cf3c283e21d5cd57d17d0a69310b88d5
Ben Boyd <beathan@gmail.com>
2013-10-22 14:41:26 -0400

    Fleshing out user profile fields

--- src/javelin/apps/core/models.py
+++ src/javelin/apps/core/models.py
@@ -47,4 +47,62 @@ AgencyUser._meta.get_field_by_name('email')[0]._unique=True
 
 
 class UserProfile(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    HAIR_COLOR_CHOICES = (
+        ('Y', 'Blonde'),
+        ('BR', 'Brown'),
+        ('BL', 'Black'),
+        ('R', 'Red'),
+        ('BA', 'Bald'),
+        ('GR', 'Gray'),
+    )
+
+    RACE_CHOICES = (
+        ('BA', 'Black/African Descent'),
+        ('WC', 'White/Caucasian'),
+        ('EI', 'East Indian'),
+        ('AS', 'Asian'),
+        ('LH', 'Latino/Hispanic'),
+        ('ME', 'Middle Eastern'),
+        ('PI', 'Pacific Islander'),
+        ('NA', 'Native American'),
+    )
+
+    RELATIONSHIP_CHOICES = (
+        ('F', 'Father'),
+        ('M', 'Mother'),
+        ('S', 'Spouse'),
+        ('BF', 'Boyfriend'),
+        ('GF', 'Girlfriend'),
+        ('B', 'Brother'),
+        ('S', 'Sister'),
+        ('FR', 'Friend'),
+        ('O', 'Other'),
+    )
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    birthday = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    hair_color = models.CharField(max_length=2, choices=HAIR_COLOR_CHOICES,
+                                  null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES,
+                              null=True, blank=True)
+    race = models.CharField(max_length=3, choices=RACE_CHOICES,
+                            null=True, blank=True)
+    height = models.CharField(max_length=10, null=True, blank=True)
+    weight = models.PositiveIntegerField(default=0, null=True, blank=True)
+    known_allergies = models.TextField(null=True, blank=True)
+    medications = models.TextField(null=True, blank=True)
+    emergency_contact_first_name = models.CharField(max_length=30, null=True,
+                                                    blank=True)
+    emergency_contact_last_name = models.CharField(max_length=30, null=True,
+                                                   blank=True)
+    emergency_contact_phone_number = models.CharField(max_length=24,
+                                                      null=True, blank=True)
+    emergency_contact_relationship =\
+        models.CharField(max_length=2, choices=RELATIONSHIP_CHOICES,
+                         null=True, blank=True)
