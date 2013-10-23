a4ee5a96752fa59eae0d75c0204bc628aa0a8f73
Ben Boyd <beathan@gmail.com>
2013-10-23 08:43:04 -0400

    Reworking routers into a module in the core app

--- src/javelin/apps/core/views.py
+++ src/javelin/apps/core/views.py
@@ -11,56 +11,35 @@ User = get_user_model()
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
 class AgencyViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Agency.objects.all()
     serializer_class = AgencySerializer
 
 
 class AlertViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Alert.objects.all()
     serializer_class = AlertSerializer
 
 
 class ChatMessageViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = ChatMessage.objects.all()
     serializer_class = ChatMessageSerializer
 
 
 class MassAlertViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = MassAlert.objects.all()
     serializer_class = MassAlertSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
--- src/javelin/urls.py
+++ src/javelin/urls.py
@@ -3,24 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from rest_framework import routers
-from core.views import (UserViewSet, GroupViewSet, AgencyViewSet, AlertViewSet,
-                        ChatMessageViewSet, MassAlertViewSet,
-                        UserProfileViewSet)
+from core.routers.v1 import router_v1
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'agencies', AgencyViewSet)
-router.register(r'alerts', AlertViewSet)
-router.register(r'chat-messages', ChatMessageViewSet)
-router.register(r'mass-alerts', MassAlertViewSet)
-router.register(r'user-profiles', UserProfileViewSet)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^api/', include(router.urls)),
-    # url(r'^$', 'javelin.views.home', name='home'),
+    url(r'^api/v1/', include(router_v1.urls)),
 )
