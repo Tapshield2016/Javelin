a601739c282514d4f6bb3268a9e03b1c6a262126
Ben Boyd <beathan@gmail.com>
2013-10-29 13:35:57 -0400

    Add register URL under v1, we may want to version these in the future

--- src/javelin/apps/core/urls.py
+++ src/javelin/apps/core/urls.py
@@ -5,7 +5,7 @@ from routers.v1 import router_v1
 
 urlpatterns = patterns('',
     url(r'^login/$', 'core.views.login'),
-    url(r'^register/$', 'core.views.register_user'),
     url(r'^resend-verification/$', 'core.views.resend_verification_email'),
+    url(r'^v1/register/$', 'core.views.register_user'),
     url(r'^v1/', include(router_v1.urls)),
 )
