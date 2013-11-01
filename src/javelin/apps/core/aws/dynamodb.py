3e92d12d6e54c90d922a4d80b45b8114c25676a6
Ben Boyd <beathan@gmail.com>
2013-11-01 08:39:03 -0400

    Move AWS service functions into classes, adds SQS broker settings for celery

--- src/javelin/apps/core/aws/dynamodb.py
+++ src/javelin/apps/core/aws/dynamodb.py
@@ -7,41 +7,41 @@ from boto.dynamodb2.table import Table
 from boto.dynamodb2.types import NUMBER
 
 
-def create_connection():
-    #if settings.DEBUG:
-    #    return DynamoDBConnection(
-    #        aws_access_key_id=settings.DYNAMO_DB_ACCESS_KEY_ID,
-    #        aws_secret_access_key=settings.DYNAMO_DB_SECRET_ACCESS_KEY,
-    #        host='0.0.0.0', port=7819)
-    #else:
-    return connect_to_region(\
-        'us-east-1',
-        aws_access_key_id=settings.DYNAMO_DB_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.DYNAMO_DB_SECRET_ACCESS_KEY)
+class DynamoDBManager(object):
 
-def create_table(table_name, read_throughput, write_throughput):
-    chat_messages = Table.create(table_name,
-            connection=create_connection(),
-            schema=[
-                HashKey('alert_id', data_type=NUMBER),
-                RangeKey('timestamp', data_type=NUMBER),
-            ],
-            throughput={
-                'read': read_throughput,
-                'write': write_throughput,
-            },
-            indexes=[
-                AllIndex('MessageTimeIndex', parts=[
-                        HashKey('alert_id'),
-                        RangeKey('timestamp'),
-                    ]),
-            ]
-        )
-    return chat_messages
+    def __init__(self):
+        self.connection = self.create_connection()
 
-def get_table(table_name):
-    return Table(table_name, connection=create_connection())
+    def create_connection(self):
+        return connect_to_region(\
+            'us-east-1',
+            aws_access_key_id=settings.DYNAMO_DB_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.DYNAMO_DB_SECRET_ACCESS_KEY)
 
-def save_item_to_table(table_name, data):
-    table = get_table(table_name)
-    table.put_item(data=data)
+    def create_table(self, table_name, read_throughput, write_throughput):
+        chat_messages =\
+            Table.create(table_name,
+                         connection=self.connection,
+                         schema=[
+                             HashKey('alert_id', data_type=NUMBER),
+                             RangeKey('timestamp', data_type=NUMBER),
+                         ],
+                         throughput={
+                             'read': read_throughput,
+                             'write': write_throughput,
+                         },
+                         indexes=[
+                             AllIndex('MessageTimeIndex', parts=[
+                             HashKey('alert_id'),
+                             RangeKey('timestamp'),
+                             ]),
+                         ]
+            )
+        return chat_messages
+
+    def get_table(self, table_name):
+        return Table(table_name, connection=self.connection)
+
+    def save_item_to_table(self, table_name, data):
+        table = self.get_table(table_name)
+        table.put_item(data=data)
--- src/javelin/apps/core/aws/sqs.py
+++ src/javelin/apps/core/aws/sqs.py
@@ -5,24 +5,24 @@ from boto.sqs.connection import SQSConnection
 from boto.sqs.message import RawMessage
 
 
-def get_sqs_connection():
-    return SQSConnection(aws_access_key_id=settings.SQS_ACCESS_KEY_ID,
-                         aws_secret_access_key=settings.SQS_SECRET_ACCESS_KEY)
+class SQSManager(object):
 
+    def __init__(self):
+        self.connection = self.get_sqs_connection()
 
-def get_queue(queue_name):
-    connection = get_sqs_connection()
-    return connection.create_queue(queue_name)
+    def get_sqs_connection(self):
+        return SQSConnection(\
+            aws_access_key_id=settings.SQS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.SQS_SECRET_ACCESS_KEY)
 
+    def get_queue(self, queue_name):
+        return self.connection.create_queue(queue_name)
 
-def get_alert_queue():
-    return get_queue(settings.SQS_ALERT_QUEUE)
+    def get_alert_queue(self):
+        return self.get_queue(settings.SQS_ALERT_QUEUE)
 
-
-def check_alert_queue():
-    queue = get_alert_queue()
-    queue.set_message_class(RawMessage)
-    result = queue.get_messages()
-    if result:
-        message = result[0]
-        return message
+    def check_alert_queue(self, num_messages=1):
+        queue = self.get_alert_queue()
+        queue.set_message_class(RawMessage)
+        messages = queue.get_messages(num_messages=num_messages)
+        return messages
