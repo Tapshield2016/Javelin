3b8f03f60f468ade62e7a357c3029464682b8479
Ben Boyd <beathan@gmail.com>
2013-11-06 09:00:40 -0500

    Unversioning registration URL, not sure there is a need

--- src/javelin/apps/core/urls.py
+++ src/javelin/apps/core/urls.py
@@ -6,6 +6,6 @@ from api.routers.v1 import router_v1
 urlpatterns = patterns('',
     url(r'^login/$', 'core.views.login'),
     url(r'^resend-verification/$', 'core.views.resend_verification_email'),
-    url(r'^v1/register/$', 'core.views.register_user'),
+    url(r'^register/$', 'core.views.register_user'),
     url(r'^v1/', include(router_v1.urls)),
 )
