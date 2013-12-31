8058d120c9edc5dfc32157dab8d993a3a924d915
Ben Boyd <beathan@gmail.com>
2013-12-31 10:38:30 -0500

    Add chat open indicator arrow, kinda looks ok

--- src/javelin/apps/shieldcommand/static/shieldcommand/js/directives.js
+++ src/javelin/apps/shieldcommand/static/shieldcommand/js/directives.js
@@ -147,7 +147,7 @@ angular.module('shieldCommand.directives', [])
 .directive('alertChatWindow', ['alertService', function(alertService) {
    return {
       restrict: 'A',
-      template: "<div class=\"alert-option chat\">\n    <i class=\"icon-chat_bubble\" ng-click=\"toggleChat()\"></i>\n    <div class=\"chat-panel panel panel-default hide\">\n        <div class=\"panel-heading\">Chat <span class=\"glyphicon glyphicon-remove pull-right\" ng-click=\"toggleChat()\"></span></div>\n        <div class=\"panel-body\">\n            <div class=\"chat-messages\">\n                <div class=\"message-container {{ isDispatcherClass(message.senderID) }}\" ng-repeat=\"message in alert.chatMessages | orderBy:'timestamp'\">\n                        <div class=\"message-content\"><span class=\"message-sender\">{{ senderName(message.senderID) }}:</span> {{ message.message }}</div>\n                        <div class=\"message-timestamp\">{{ message.timestamp * 1000 | date:'MM-dd HH:mm:ss' }}</div>\n                </div>\n            </div>\n            <div class=\"message-box\">\n                <textarea ng-model=\"newChatMessage\" placeholder=\"Enter message here...\"></textarea>\n            </div> \n        </div> \n    </div>\n</div>",
+      template: "<div class=\"alert-option chat\">\n    <i class=\"icon-chat_bubble\" ng-click=\"toggleChat()\"></i>\n   <div class=\"arrow-left hide\"></div> <div class=\"chat-panel panel panel-default hide\">\n        <div class=\"panel-heading\">Chat <span class=\"glyphicon glyphicon-remove pull-right\" ng-click=\"toggleChat()\"></span></div>\n        <div class=\"panel-body\">\n            <div class=\"chat-messages\">\n                <div class=\"message-container {{ isDispatcherClass(message.senderID) }}\" ng-repeat=\"message in alert.chatMessages | orderBy:'timestamp'\">\n                        <div class=\"message-content\"><span class=\"message-sender\">{{ senderName(message.senderID) }}:</span> {{ message.message }}</div>\n                        <div class=\"message-timestamp\">{{ message.timestamp * 1000 | date:'MM-dd HH:mm:ss' }}</div>\n                </div>\n            </div>\n            <div class=\"message-box\">\n                <textarea ng-model=\"newChatMessage\" placeholder=\"Enter message here...\"></textarea>\n            </div> \n        </div> \n    </div>\n</div>",
       scope: {
         alert: "=",
       },
@@ -176,6 +176,7 @@ angular.module('shieldCommand.directives', [])
 
         scope.toggleChat = function() {
           element.find('.chat-panel').toggleClass('hide');
+          element.find('.arrow-left').toggleClass('hide');
           if (scope.alert.hasNewChatMessage) {
             scope.alert.hasNewChatMessage = false;
           };
