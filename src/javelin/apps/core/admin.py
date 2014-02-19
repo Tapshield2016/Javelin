a776ea05581f2d75de175deb4a1c58367e28a385
Ben Boyd <beathan@gmail.com>
2014-02-19 18:59:41 +0000

    Switch to OSMGeoAdmin for Agency model, refs #120

--- src/javelin/apps/core/admin.py
+++ src/javelin/apps/core/admin.py
@@ -1,12 +1,13 @@
 import reversion
 
 from django.contrib import admin
+from django.contrib.gis import admin as geo_admin
 
 from models import (Agency, AgencyUser, Alert, AlertLocation, MassAlert,
                     ChatMessage, UserProfile)
 
 
-class AgencyAdmin(reversion.VersionAdmin):
+class AgencyAdmin(reversion.VersionAdmin, geo_admin.OSMGeoAdmin):
     pass
 
 
