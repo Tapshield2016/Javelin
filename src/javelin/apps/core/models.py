27916de9f8018a12540b62e7d7086f94e6b40a6e
Ben Boyd <beathan@gmail.com>
2014-03-12 17:38:05 -0400

    Add fields for theming, resolves #141

--- src/javelin/apps/core/models.py
+++ src/javelin/apps/core/models.py
@@ -71,6 +71,14 @@ class Agency(TimeStampedModel):
         models.TextField(null=True, blank=True,
                          default=DEFAULT_AUTORESPONDER_MESSAGE,
                          verbose_name="chat auto-responder message")
+    agency_logo = models.URLField(null=True, blank=True,
+                                  help_text="Set the location of the standard agency logo.")
+    agency_alternate_logo = models.URLField(null=True, blank=True,
+                                            help_text="This could be an inverted version of the standard logo or other differently colorized/formatted version.")
+    agency_small_logo = models.URLField(null=True, blank=True,
+                                        help_text="This could be a truncated or minimized form of the logo, e.g. 'UF' versus the larger logo version.")
+    agency_theme = models.TextField(null=True, blank=True, default="{}",
+                                    help_text="Use properly formatted JSON here to provide data as necessary.")
 
     objects = models.Manager()
     geo = db_models.GeoManager()
