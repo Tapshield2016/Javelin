3d1acd7536168979676c933d6e032653a491cbb9
Adam Share <adam@tapshield.com>
2014-06-09 01:43:53 -0400

    .html response

--- src/javelin/apps/emailmgr/views.py
+++ src/javelin/apps/emailmgr/views.py
@@ -131,7 +131,7 @@ def email_activate(request, identifier="somekey"):
             user_activated_email.send(sender=EmailAddress, email_address=email)
             Msg.add_message (request, Msg.SUCCESS, _('email address is now active'))
             
-    return HttpResponseRedirect(reverse('emailmgr_email_list'))
+    return render_to_response(get_template('emailmgr_email_list.html'))
 
 @api_view(['POST'])
 def email_delete(request, identifier="somekey"):
