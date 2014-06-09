80577d05cb20d476b93bdcbf4670a04bba398a33
Adam Share <adam@tapshield.com>
2014-06-09 01:25:53 -0400

    temp adjust

--- src/javelin/apps/emailmgr/defaults.py
+++ src/javelin/apps/emailmgr/defaults.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-EMAIL_MGR_TEMPLATE_PATH = getattr(settings, "EMAIL_MGR_TEMPLATE_PATH", "emailmgr")
+EMAIL_MGR_TEMPLATE_PATH = getattr(settings, "EMAIL_MGR_TEMPLATE_PATH")
 
 
 
