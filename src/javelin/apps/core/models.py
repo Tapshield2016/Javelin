a582fc19b2dc3de6c01ea37deb6b09c4b2b21b1d
Adam Share <adam@tapshield.com>
2014-06-14 16:39:35 -0400

    limit choices to pk

--- src/javelin/apps/core/models.py
+++ src/javelin/apps/core/models.py
@@ -157,7 +157,7 @@ class Region(models.Model):
     name = models.CharField(max_length=255)
     primary_dispatch_center = models.ForeignKey('DispatchCenter',
                                                 related_name='primary_dispatch_center',
-                                                limit_choices_to={'agency': agency})
+                                                limit_choices_to={'agency': agency.pk})
     secondary_dispatch_center = models.ForeignKey('DispatchCenter',
                                                   related_name='secondary_dispatch_center',
                                                   null=True, blank=True)
