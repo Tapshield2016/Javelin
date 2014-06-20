c062002b5ed637a3255b868dc3a2409c5ac6aca8
Adam Share <adam@tapshield.com>
2014-06-20 18:02:03 -0400

    try new encoder

--- src/javelin/apps/core/views.py
+++ src/javelin/apps/core/views.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 from time import mktime
-
+from django.core.serializers.json import DjangoJSONEncoder
 from django.conf import settings
 from django.contrib.auth import (authenticate, get_user_model,
                                  login as auth_login)
@@ -51,8 +51,8 @@ User = get_user_model()
 class DatetimeEncoder(json.JSONEncoder):
 
     def default(self, obj):
-        # if isinstance(obj, datetime.datetime):
-        #     return int(mktime(obj.timetuple()))
+        if isinstance(obj, datetime.datetime):
+            return int(mktime(obj.timetuple()))
 
         return json.JSONEncoder.default(self, obj)
 
@@ -214,7 +214,7 @@ def login(request):
         if request.user.agency:
             serialized.data['agency'] =\
                 AgencySerializer(request.user.agency).data
-        message = json.dumps(serialized.data, cls=DatetimeEncoder)
+        message = json.dumps(serialized.data, cls=DjangoJSONEncoder)
     else:
         status = 401
         message = "Authorization required."
