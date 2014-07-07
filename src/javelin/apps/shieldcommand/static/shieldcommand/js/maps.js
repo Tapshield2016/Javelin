30591564374a004933de8bb6928084b026a694ed
Adam Share <adam@tapshield.com>
2014-07-07 17:15:11 -0400

    spotcrime time string remove seconds

--- src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
+++ src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
@@ -267,7 +267,7 @@ function spotCrimePinClicked(evt)
 				var infoContent = '<h4 style="margin-top: 0">' + marker.title + '</h4>' +
 				'<div id="' + contentID + '">' +
 				'<table class="table table-condensed">' +
-				'<tr><td><strong>Date</td><td>' + date.toLocaleString(navigator.language, {hour: '2-digit', minute:'2-digit'}) + '</td></tr>' +
+				'<tr><td><strong>Date</td><td>' + date.toLocaleDateString() date.toLocaleString(navigator.language, {hour: '2-digit', minute:'2-digit'}) + '</td></tr>' +
 				'<tr><td><strong>Address</td><td>' + spotCrime.address + '</td></tr>' +
 				'</table>' +
 				'<p style="margin-top: 10px;"><a class="btn btn-info" href="' + spotCrime.link + '" target="_blank">More Info</a></p>' +
