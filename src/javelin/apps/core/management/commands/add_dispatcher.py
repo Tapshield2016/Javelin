e8571592096519d36da6c23d8ff65270687dfac3
Adam Share <adam@tapshield.com>
2014-08-26 17:32:56 -0400

    add dispatcher

--- src/javelin/apps/core/management/commands/add_dispatcher.py
+++ src/javelin/apps/core/management/commands/add_dispatcher.py
@@ -4,20 +4,36 @@ import string
 import random
 from django.contrib.auth.models import Group
 from core.models import AgencyUser, Agency
+from optparse import make_option
+from django.core.management.base import BaseCommand
 
-def register_user(first_name, last_name, email, agencyname):
 
-    user_group = Group.objects.get(name='Dispatchers')
-    agency = Agency.objects.get(name=agencyname) # set agency name
-# users = ["Jessica,Zarate,jzarate@ufl.edu",] # set users list
-    for user in users:
+class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('--agency',
+                    dest='agency',
+                    help='The numerical ID or name of the agency to add dispatcher'),
+        make_option('--email',
+                    dest='email',
+                    help='Email of the dispatcher user to add'),
+    )
+
+    def handle(self, *args, **options):
+        user_group = Group.objects.get(name='Dispatchers')
+        email = options['email']
+        agency_id = int(options['agency'])
+        agency = Agency.objects.get(pk=agency_id)
+
+        if not agency:
+            agency_name = options['agency']
+            agency = Agency.objects.get(name=agency_name)
+
         password = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(6)])
         print "%s, %s" % (email, password)
         new_user = AgencyUser.objects.create_user(email, email, password)
         new_user.groups.add(user_group)
         new_user.agency = agency
-        new_user.first_name = first_name.title()
-        new_user.last_name = last_name.title()
         new_user.disarm_code = 0000
         new_user.phone_number = '(555) 555-5555'
         new_user.is_active = True
