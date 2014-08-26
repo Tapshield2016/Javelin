8ea6e6097e01e9a1b99c19af79d76d542baa0ce0
Adam Share <adam@tapshield.com>
2014-08-26 17:45:56 -0400

    add dispatcher

--- src/javelin/apps/core/management/commands/add_dispatcher.py
+++ src/javelin/apps/core/management/commands/add_dispatcher.py
@@ -17,11 +17,14 @@ class Command(BaseCommand):
         make_option('--email',
                     dest='email',
                     help='Email of the dispatcher user to add'),
+        make_option('--list',
+                    dest='list',
+                    help='Email of the dispatcher user to add "first,last,email"'),
     )
 
     def handle(self, *args, **options):
+
         user_group = Group.objects.get(name='Dispatchers')
-        email = options['email']
         agency_name = options['agency']
         agency = Agency.objects.get(name=agency_name)
 
@@ -29,13 +32,27 @@ class Command(BaseCommand):
             agency_id = int(options['agency'])
             agency = Agency.objects.get(pk=agency_id)
 
-        password = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(6)])
-        print "%s, %s" % (email, password)
-        new_user = AgencyUser.objects.create_user(email, email, password)
-        new_user.groups.add(user_group)
-        new_user.agency = agency
-        new_user.disarm_code = 0000
-        new_user.phone_number = '(555) 555-5555'
-        new_user.is_active = True
-        new_user.email_verified = True
-        new_user.save()
+        list = options['list']
+        if not list:
+            list = []
+            email = options['email']
+            list.append(" , ,"+email)
+
+        for user in list:
+            tokens = user.split(',')
+            first_name = tokens[0].strip()
+            last_name = tokens[1].strip()
+            email = tokens[2].strip()
+
+            password = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(6)])
+            print "%s, %s" % (email, password)
+            new_user = AgencyUser.objects.create_user(email, email, password)
+            new_user.groups.add(user_group)
+            new_user.agency = agency
+            new_user.first_name = first_name.title()
+            new_user.last_name = last_name.title()
+            new_user.disarm_code = 0000
+            new_user.phone_number = '(555) 555-5555'
+            new_user.is_active = True
+            new_user.email_verified = True
+            new_user.save()
