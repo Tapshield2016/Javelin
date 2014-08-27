df06306bdd8eebe5db1b35e00c034527f72d4f35
Adam Share <adam@tapshield.com>
2014-08-27 16:59:20 -0400

    static device

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -12,7 +12,8 @@ from django.contrib.auth.models import Group
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from django.core.mail import send_mail
-
+from core.api.routers.v1 import
+from rest_framework.reverse import reverse
 from django.http import HttpResponse
 
 from rest_framework import status, viewsets, ISO_8601
@@ -509,7 +510,7 @@ class StaticDeviceViewSet(viewsets.ModelViewSet):
         if agency_id:
             agency = get_agency_from_unknown(agency_id)
         if agency:
-            request_data['agency'] = agency.id
+            request_data['agency'] = reverse('agency', args=[agency.id], request=request)
 
         serializer = self.get_serializer(data=request_data, files=request.FILES)
 
