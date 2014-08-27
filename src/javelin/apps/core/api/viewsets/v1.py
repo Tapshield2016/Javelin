8b30ae722d33236fe9d1219f1c4166c9dc60d534
Adam Share <adam@tapshield.com>
2014-08-27 18:05:56 -0400

    static device

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -509,7 +509,7 @@ class StaticDeviceViewSet(viewsets.ModelViewSet):
         if agency_id:
             agency = get_agency_from_unknown(agency_id)
         if agency:
-            request_data['agency'] = agency.url
+            request_data['agency'] = AgencySerializer(agency)['url']
 
         serializer = self.get_serializer(data=request_data, files=request.FILES)
 
