dfd9d32f085cef050b710794cf00466aed529ec4
Adam Share <adam@tapshield.com>
2014-08-27 23:24:50 -0400

    static device

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -534,9 +534,9 @@ class DispatchCenterViewSet(viewsets.ModelViewSet):
 
 class StaticDeviceViewSet(viewsets.ModelViewSet):
     permission_classes = (IsOwnerOrReadOnly, DeviceMakerOnly)
-    queryset = StaticDevice.objects.select_related('agency').all()
+    # queryset = StaticDevice.objects.select_related('agency').all()
     serializer_class = StaticDeviceSerializer
-    filter_fields = ('agency',)
+    # filter_fields = ('agency',)
 
     def create(self, request):
 
