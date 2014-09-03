dd9953fab4c5f8dd99a1a8a340b0d290e30825d2
Adam Share <adam@tapshield.com>
2014-09-02 23:58:32 -0400

    image path on to native them

--- src/javelin/apps/core/api/serializers/v1.py
+++ src/javelin/apps/core/api/serializers/v1.py
@@ -215,9 +215,9 @@ class ThemeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Theme
 
-    # def to_native(self, obj):
-    #     ret = super(ThemeSerializer, self).to_native(obj)
-    #     if obj:
-    #         if obj.logo:
-    #             ret['logo_url'] = '%s%s' % settings.AWS_S3_BUCKET_URL, obj.logo.name
-    #     return ret
+    def to_native(self, obj):
+        ret = super(ThemeSerializer, self).to_native(obj)
+        if obj:
+            if obj.logo:
+                ret['logo_url'] = '%s%s' % settings.AWS_S3_BUCKET_URL, obj.logo.name
+        return ret
