0c3413848d1e4c03f091e8c4746b41cf8aef571b
Adam Share <adam@tapshield.com>
2014-09-04 04:25:42 -0400

    static device type default

--- src/javelin/apps/core/models.py
+++ src/javelin/apps/core/models.py
@@ -715,7 +715,7 @@ class StaticDevice(models.Model):
 
     uuid = models.SlugField(max_length=255, unique=True,
                             help_text="Unique identifier (e.g. serial number)")
-    type = models.CharField(max_length=255, default="Static Device",
+    type = models.CharField(max_length=255, blank=True, default="Static Device",
                             help_text="Model number or device type")
     description = models.CharField(max_length=255, null=True, blank=True,
                                    help_text="Human readable identifier denoting location "
