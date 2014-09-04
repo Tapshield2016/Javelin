18039b07b5e5d34301122509b36d204d2cb5e395
Adam Share <adam@tapshield.com>
2014-09-04 19:10:39 -0400

    static icons

--- src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
+++ src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
@@ -117,9 +117,6 @@ function getIconForLocation(location) {
 			
 			icon = location.type.toLowerCase() + '/' + 'pins_' + crimeType + '_icon.png';
 		}
-        else if (location.type == 'staticAlert') {
-            icon = 'static_marker.png'
-        }
     }
     
 	return '/media/static/shieldcommand/img/' + icon;
