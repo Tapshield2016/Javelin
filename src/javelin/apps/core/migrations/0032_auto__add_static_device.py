9a3cd745f92d37dca04530c72afb11aca2d6ba31
Adam Share <adam@tapshield.com>
2014-09-04 05:37:47 -0400

    migration refactor

--- src/javelin/apps/core/migrations/0032_auto__add_static_device.py
+++ src/javelin/apps/core/migrations/0032_auto__add_static_device.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("core", "0031_auto__add_agency__spot_crime_days_visible"),
+    )
+
     def forwards(self, orm):
         # Adding model 'StaticDevice'
         db.create_table(u'core_staticdevice', (
