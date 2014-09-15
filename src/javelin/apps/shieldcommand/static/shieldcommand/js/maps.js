69b7bc929d1936010499ae42908d2e30365fbd6e
Adam Share <adam@tapshield.com>
2014-09-14 21:36:22 -0400

    marker clusterer icon

--- src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
+++ src/javelin/apps/shieldcommand/static/shieldcommand/js/maps.js
@@ -74,8 +74,8 @@ function initializeMap() {
 
     var style = [{
         url: '/media/static/shieldcommand/img/spotcrime/'+'pins_cluster_red.png',
-        height: 60,
-        width: 40,
+        height: 50,
+        width: 45,
         anchor: [10,15],
         textColor: '#d2322d',
         textSize: 10
