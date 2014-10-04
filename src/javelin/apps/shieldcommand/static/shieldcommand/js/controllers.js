a7a9737ac4764b752be3d6752b849f11bcf0129b
Adam Share <adam@tapshield.com>
2014-10-04 12:55:25 -0400

    no alerts bool

--- src/javelin/apps/shieldcommand/static/shieldcommand/js/controllers.js
+++ src/javelin/apps/shieldcommand/static/shieldcommand/js/controllers.js
@@ -439,7 +439,7 @@ angular.module('shieldCommand.controllers', [])
     $scope.unviewedCrimeTipsLength = 0;
 	$scope.crimeTipUpdateInterval = 20;
 	$scope.markerSetForActiveCrimeTip = false;
-    $scope.alertsAvailable = false;
+    $scope.alertsAvailable = true;
 
 	$scope.$on('alertMarkedChange', function() {
 		updateDisplay();
