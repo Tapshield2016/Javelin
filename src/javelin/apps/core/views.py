c86549c4fb25beb896d50434ef35ecb2a9a1ad18
Adam Share <adam@tapshield.com>
2014-10-08 21:06:34 -0400

    direct api call for chat

--- src/javelin/apps/core/views.py
+++ src/javelin/apps/core/views.py
@@ -690,8 +690,8 @@ def create_alert(request):
     request_data = request.DATA.copy()
 
     if request_data:
-        request.DATA['user'] = request.user.username
-        alert_ok = new_alert(request.DATA)
+        request_data['user'] = request.user.username
+        # alert_ok = new_alert(request_data)
         active_alerts = Alert.active.filter(agency_user=request.user)
         if (alert_ok):
             if active_alerts:
