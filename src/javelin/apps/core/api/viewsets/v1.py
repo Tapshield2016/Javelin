6f0e2e5491be9828aa60959d033d57a1fbe04c65
Adam Share <adam@tapshield.com>
2014-10-09 22:17:21 -0400

    new completion url

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -473,9 +473,13 @@ class AlertViewSet(viewsets.ModelViewSet):
                                       'timestamp': time.time(),
                                       'message_id': message_id})
 
+        push_notification_message = alert.agency.alert_completed_message
+        if alert.initiated_by == "C":
+            push_notification_message = "Dispatcher has ended your chat session"
+
         if not request.user.id == alert.agency_user.id:
             user = alert.agency_user
-            notify_alert_completed.delay(alert.agency.alert_completed_message, user.device_type, user.device_endpoint_arn)
+            notify_alert_completed.delay(push_notification_message, user.device_type, user.device_endpoint_arn)
 
         return Response(serialized.data)
 
