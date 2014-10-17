e984a7498a86fb85dafd57bf77396c1385e45d92
Adam Share <adam@tapshield.com>
2014-10-17 01:27:25 -0400

    add entourage members json

--- src/javelin/apps/core/views.py
+++ src/javelin/apps/core/views.py
@@ -719,16 +719,16 @@ def set_entourage_members(request):
 
     entourage_members = json.loads(json_data['json'])
 
-    return Response("ok",
+    return Response({"message": "ok"},
                     status=status.HTTP_200_OK)
 # data is now a Python list or dict representing the uploaded JSON.
 
-    for member in entourage_members:
-        return Response("ok",
-                    status=status.HTTP_200_OK)
-        new_member = EntourageMemberUpdateSerializer(data=member)
-        new_member.user = request.user
-        new_member.save()
-
-    return Response(UserSerializer(instance=request.user).data,
-                    status=status.HTTP_200_OK)
+    # for member in entourage_members:
+    #     return Response({"message": "ok"},
+    #                     status=status.HTTP_200_OK)
+    #     new_member = EntourageMemberUpdateSerializer(data=member)
+    #     new_member.user = request.user
+    #     new_member.save()
+    #
+    # return Response(UserSerializer(instance=request.user).data,
+    #                 status=status.HTTP_200_OK)
