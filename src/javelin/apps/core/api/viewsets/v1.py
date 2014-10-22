461592ee1b02f19c8203b10a71e9ca17fed5246b
Adam Share <adam@tapshield.com>
2014-10-21 22:20:16 -0400

    matched_entourage_users

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -360,7 +360,11 @@ class UserViewSet(viewsets.ModelViewSet):
         serialized_tracking = UserTrackingEntourageMemberSerializer(tracking_users, many=True)
         serialized_no_tracking = UserNoLocationEntourageMemberSerializer(no_tracking_users, many=True)
 
-        return Response({"users": [serialized_always.data, serialized_tracking.data, serialized_no_tracking.data]},
+        serialized_data = UserAlwaysVisibleEntourageMemberSerializer(always_visible_users, many=True).data
+        serialized_data.append(UserTrackingEntourageMemberSerializer(tracking_users, many=True).data)
+        serialized_data.append(UserNoLocationEntourageMemberSerializer(no_tracking_users, many=True).data)
+
+        return Response({"users": serialized_data},
                         status=status.HTTP_200_OK)
 
 
