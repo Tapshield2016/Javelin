facb27bc393db701ab76832e51c318804a1a05ef
Adam Share <adam@tapshield.com>
2014-11-17 16:01:33 -0500

    notification

--- src/javelin/apps/core/models.py
+++ src/javelin/apps/core/models.py
@@ -39,7 +39,7 @@ from managers import (ActiveAlertManager, InactiveAlertManager,
 
 from core.aws.s3_filefield import S3EnabledImageField, S3URLField
 
-from notifications import added_by_user_message
+# from notifications import added_by_user_message
 
 from tasks import (notify_user_added_to_entourage,
                    notify_user_called_emergency_number,
