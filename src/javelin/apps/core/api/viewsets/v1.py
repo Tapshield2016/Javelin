207ceadb513684ae20eed1506aef088a25765304
Adam Share <adam@tapshield.com>
2014-11-21 11:37:01 -0500

    super user user queryset

--- src/javelin/apps/core/api/viewsets/v1.py
+++ src/javelin/apps/core/api/viewsets/v1.py
@@ -501,9 +501,9 @@ class AgencyViewSet(viewsets.ModelViewSet):
             # We got one or more values but not all we need, so return none
             qs = Agency.objects.none()
 
-        if self.request.user.is_superuser:
-            return qs
-        return qs.exclude(hidden=True)
+        # if self.request.user.is_superuser:
+        return qs
+        # return qs.exclude(hidden=True)
 
     @detail_route(methods=['post'])
     def send_mass_alert(self, request, pk=None):
