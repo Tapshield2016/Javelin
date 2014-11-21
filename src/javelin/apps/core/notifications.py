b7ca6d5c1a14efd32aa70701e81665a7d1332f3d
Adam Share <adam@tapshield.com>
2014-11-21 15:23:43 -0500

    super user user queryset

--- src/javelin/apps/core/notifications.py
+++ src/javelin/apps/core/notifications.py
@@ -19,7 +19,7 @@ def added_by_user_message(user):
 
 def called_emergency_number_message(user):
 
-    return u"%s dialed 911. They may be in need of assistance." % readable_name_for_user(user)
+    return u"%s dialed 911.\\nThey may be in need of assistance." % readable_name_for_user(user)
 
 
 def called_emergency_number_subject(user):
