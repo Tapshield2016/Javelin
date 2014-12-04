ee8dd20038c9b05f0f724bc97cdc50e55b6999ce
Adam Share <adam@tapshield.com>
2014-12-04 04:45:28 -0500

    html forms

--- src/javelin/apps/shieldcommand/views.py
+++ src/javelin/apps/shieldcommand/views.py
@@ -7,22 +7,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from core.api.serializers.v1 import UserSerializer, ThemeSerializer
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
 
 
 @login_required(login_url='shieldcommand-login')
