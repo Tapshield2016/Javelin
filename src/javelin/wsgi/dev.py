5498eda608d403f1e1e5e0be4c1cde297511f5d2
Adam Share <ashare80@gmail.com>
2016-04-29 23:50:45 -0700

    wsgi remove

--- src/javelin/wsgi/dev.py
+++ src/javelin/wsgi/dev.py
@@ -14,10 +14,10 @@ framework.
 
 """
 import os
-import sys
-
-sys.path.append('/srv/www/javelin/src')
-sys.path.append('/srv/www/javelin/src/javelin')
+# import sys
+#
+# sys.path.append('/srv/www/javelin/src')
+# sys.path.append('/srv/www/javelin/src/javelin')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
